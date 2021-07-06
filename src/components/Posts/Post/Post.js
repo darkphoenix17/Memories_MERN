7d8227d18b243a53fc01ed28d1a2624345862a98
darkphoenix17 <karanmistry284@gmail.com>
2021-07-06 05:52:56 +0530

    Like Post Functionality Added

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -5,7 +5,7 @@ import moment from "moment";
 import useStyles from './styles';
 import { useDispatch } from 'react-redux';
 
-import { deletePost } from "../../../actions/posts";
+import { deletePost, likePost } from "../../../actions/posts";
 
 const Post = ({ post, setCurrentId }) => {
     const classes = useStyles();
@@ -28,8 +28,8 @@ const Post = ({ post, setCurrentId }) => {
                 <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" onClick={() => { }}><MdThumbUp fontSize="default" />  Like {post.likeCount} </Button>
-                <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}><MdDelete fontSize="default" />  Delete</Button>
+                <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}><MdThumbUp fontSize="default" /> &nbsp; Like &nbsp; {post.likeCount} </Button>
+                <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}><MdDelete fontSize="default" />  &nbsp; Delete &nbsp;</Button>
             </CardActions>
         </Card>
     );
